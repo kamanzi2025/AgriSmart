88af90284e47b2bf195ec2800d090c4818a9464d
kamanzi2025 <g.kamanzi1@alustudent.com>
2026-03-28 11:43:38 +0200

    Add app entry point

---
+++ root/main.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from forms.login import LoginWindow
+
+if __name__ == "__main__":
+    app = LoginWindow()
+    app.mainloop()
